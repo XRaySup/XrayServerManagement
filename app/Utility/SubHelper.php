efe2ce95ad294ad486cb680546b61a7b048f12a5
XraySup <xray.supp@gmail.com>
2024-04-02 23:42:48 +0100

    main

--- app/Utility/SubHelper.php
+++ app/Utility/SubHelper.php
@@ -118,14 +118,14 @@ function genMultiServersJson($servers)
     $jsonContent = json_encode($sampleLeastPing, JSON_PRETTY_PRINT);
 
     // Write JSON content to file
-
-    file_put_contents('storage/multiConfLeastPing.json', $jsonContent);
+    
+    file_put_contents(storage_path('app/public/storage/multiConfLeastPing.json'), $jsonContent);
 
     $jsonContent = json_encode($JsonConfigs, JSON_PRETTY_PRINT);
 
     //  Write JSON content to file
-
-    file_put_contents('storage/multiConf.json', $jsonContent);
+    
+    file_put_contents(storage_path('app/public/storage/multiConf.json'), $jsonContent);
 
 
 }
@@ -167,9 +167,8 @@ function genMultiServersLink($servers)
 
 
     }
-
-
-    file_put_contents('storage/links.sub', $serversLinks);
+    
+    file_put_contents(storage_path('app/public/storage/links.sub'), $serversLinks);
 }
 function getClientOutbounds($inbound, $client, $address)
 {
