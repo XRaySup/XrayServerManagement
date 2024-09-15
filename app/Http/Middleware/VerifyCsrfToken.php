c77f5d074caf67a62dbbfd6455d25649b1b93d8e
XraySup <xray.supp@gmail.com>
2024-09-15 02:22:18 +0100

    main

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,7 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        //
+        // Exclude the Telegram webhook route from CSRF protection
+        'isegarobot/webhook',
     ];
 }
