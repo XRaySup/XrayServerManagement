6ce13f8515f9e7a577d5c0693862fbaa9379981e
XraySup <xray.supp@gmail.com>
2024-09-15 04:00:08 +0100

    main

--- config/telegram.php
+++ config/telegram.php
@@ -31,7 +31,7 @@ return [
     |             ]
     */
     'bots' => [
-        'isegarobot' => [
+        'mybot' => [
             'token' => env('TELEGRAM_BOT_TOKEN', 'YOUR-BOT-TOKEN'),
             'certificate_path' => env('TELEGRAM_CERTIFICATE_PATH', 'YOUR-CERTIFICATE-PATH'),
             'webhook_url' => env('TELEGRAM_WEBHOOK_URL', 'YOUR-BOT-WEBHOOK-URL'),
