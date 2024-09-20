1a09c212dcbc01013a585a384d96c8e8048aeba6
XraySup <xray.supp@gmail.com>
2024-09-20 23:21:21 +0100

    main

--- app/Filament/Widgets/ServersUsage.php
+++ app/Filament/Widgets/ServersUsage.php
@@ -4,10 +4,9 @@ namespace App\Filament\Widgets;
 
 use App\Filament\Resources\ServerResource;
 use App\Models\Server;
-use App\Models\Usage;
-use Carbon\Carbon;
-use Filament\Tables\Columns\TextColumn;
+//use Filament\Tables;
 use Filament\Tables\Columns\Summarizers\Summarizer;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Filters\SelectFilter;
 use Filament\Tables\Table;
 use Filament\Widgets\TableWidget as BaseWidget;
@@ -15,48 +14,46 @@ use Filament\Widgets\TableWidget as BaseWidget;
 class ServersUsage extends BaseWidget
 {
     protected int | string | array $columnSpan = "full";
-
     public function table(Table $table): Table
     {
-        // Aggregate the total usage from the Usage model instead of Server model
-        $totalTodayUsage = Usage::selectRaw('SUM(upIncrease + downIncrease) / 1024 / 1024 / 1024 as totalTodayUsage')
-            ->whereBetween('created_at', [Carbon::today(), now()])
-            ->value('totalTodayUsage');
 
-        $totalYesterdayUsage = Usage::selectRaw('SUM(upIncrease + downIncrease) / 1024 / 1024 / 1024 as totalYesterdayUsage')
-            ->whereBetween('created_at', [Carbon::yesterday(), Carbon::today()])
-            ->value('totalYesterdayUsage');
+        $servers = Server::all();
+        $totalTodayUsage = 0;
+        $totalYesterdayUsage = 0;
+        $totalWeeklyUsage = 0;
 
-        $totalWeeklyUsage = Usage::selectRaw('SUM(upIncrease + downIncrease) / 1024 / 1024 / 1024 as totalWeeklyUsage')
-            ->whereBetween('created_at', [Carbon::now()->subWeek(), Carbon::today()])
-            ->value('totalWeeklyUsage');
+        foreach ($servers as $server) {
+
+            $totalTodayUsage += $server->todayUsage;
+            $totalYesterdayUsage += $server->yesterdayUsage;
+            $totalWeeklyUsage += $server->weeklyUsage;
+        }
+
 
         return $table
             ->query(ServerResource::getEloquentQuery())
             ->columns([
+
                 TextColumn::make('remark')
                     ->searchable()
                     ->sortable(),
                 TextColumn::make('today usage')
                     ->sortable()
-                    ->label('Today Usage')
                     ->summarize(Summarizer::make()
-                        ->label('Total: ' . round($totalTodayUsage, 1) . ' GB')),
+                        ->label($totalTodayUsage)),
                 TextColumn::make('yesterday usage')
                     ->sortable()
-                    ->label('Yesterday Usage')
                     ->summarize(Summarizer::make()
-                        ->label('Total: ' . round($totalYesterdayUsage, 1) . ' GB')),
+                        ->label($totalYesterdayUsage)),
                 TextColumn::make('weekly usage')
                     ->sortable()
-                    ->label('Weekly Usage')
                     ->summarize(Summarizer::make()
-                        ->label('Total: ' . round($totalWeeklyUsage, 1) . ' GB')),
+                        ->label($totalWeeklyUsage))
             ])
             ->filters([
                 SelectFilter::make('status')
                     ->options(Server::stats())
                     ->default('ONLINE')
-            ]);   
+            ]);
     }
 }
