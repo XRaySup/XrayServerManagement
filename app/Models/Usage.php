c8264e8ce31988260b3165a47dc1b034a25bc5eb
XraySup <xray.supp@gmail.com>
2024-09-21 00:05:50 +0100

    main

--- app/Models/Usage.php
+++ app/Models/Usage.php
@@ -9,5 +9,23 @@ class Usage extends Model
 {
     //use HasFactory;
     protected $fillable = ['server_id','inbound_id','client_id','up','down','upIncrease','downIncrease', 'timestamps'];
-
+    public static function cleanUpAllRecords()
+    {
+        $deletedCount = 0;
+    
+        $records = self::select('server_id', 'inbound_id', 'client_id', \DB::raw('DATE(created_at) as date'), \DB::raw('MAX(created_at) as max_created_at'))
+            ->groupBy('server_id', 'inbound_id', 'client_id', 'date')
+            ->get();
+    
+        foreach ($records as $record) {
+            $deletedCount += self::where('server_id', $record->server_id)
+                ->where('inbound_id', $record->inbound_id)
+                ->where('client_id', $record->client_id)
+                ->whereDate('created_at', $record->date)
+                ->where('created_at', '<', $record->max_created_at)
+                ->delete();
+        }
+    
+        return $deletedCount; // Return the number of deleted records
+    }
 }
