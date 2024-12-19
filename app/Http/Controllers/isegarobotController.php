6ea516a249d5bc9417d4370e46f53d18d2a939f2
XRaySup <xray.supp@gmail.com>
2024-12-19 22:12:41 +0000

    main

--- app/Http/Controllers/isegarobotController.php
+++ app/Http/Controllers/isegarobotController.php
@@ -30,34 +30,34 @@ class isegarobotController extends Controller
         $userName = $message['from']['username'] ?? 'Unknown';
         $firstName = $message['from']['first_name'] ?? 'Unknown';
         $lastName = $message['from']['last_name'] ?? 'Unknown';
-    
+
         $adminIds = explode(',', env('TELEGRAM_ADMIN_IDS'));
-    
-        if (!in_array((int)$chatId, $adminIds)) {
+
+        if (!in_array((int) $chatId, $adminIds)) {
             // Notify admins about the unauthorized attempt
             foreach ($adminIds as $adminId) {
                 $this->sendReply(trim($adminId), null, "Non-admin user tried to interact: \nID: $chatId\nUsername: $userName\nName: $firstName $lastName");
             }
-    
+
             // Send message to the non-admin user
             $this->sendReply($chatId, $messageId, "You are not authorized to use this bot.");
-            
+
             // Return response after non-admin check
             return response()->json(['status' => 'ok']);
         }
-    
+
         // Continue if the user is an admin
         if (isset($message['document'])) {
             $fileId = $message['document']['file_id'];
-    
+
             // Send initial message about processing start
             $initialReply = "Your file is being processed.";
             $progressMessage = $this->sendReply($chatId, $messageId, $initialReply);
-    
+
             if (!$progressMessage) {
                 return response()->json(['status' => 'error', 'message' => 'Failed to send initial reply'], 500);
             }
-            
+
             try {
                 // Download the file from Telegram
                 $file = $this->telegram->getFile(['file_id' => $fileId]);
@@ -65,12 +65,12 @@ class isegarobotController extends Controller
                 $fileUrl = "https://api.telegram.org/file/bot" . env('TELEGRAM_BOT_TOKEN') . "/$filePath";
                 $fileContents = Http::get($fileUrl)->body();
                 //Log::info('File contents: ' . print_r($fileContents, true));
-    
+
                 // Dispatch a single job with the entire file contents
-                
+
                 ProcessIpsJob::dispatch($fileContents, $progressMessage);
                 //$progressMessage = $this->sendReply($chatId, $messageId, 'test');
-                
+
             } catch (\Telegram\Bot\Exceptions\TelegramResponseException $e) {
                 Log::error('Telegram API error: ' . $e->getMessage());
                 $this->sendReply($chatId, $messageId, "Error: {$e->getMessage()}");
@@ -80,10 +80,17 @@ class isegarobotController extends Controller
             }
         } else {
             if (isset($message['text'])) {
-                if ($message['text'] === 'Run') {
-                    //$this->sendReply($chatId, $messageId, "before DNS update command has been executed.");
-                    Artisan::call('dns:update');
-                    //$this->sendReply($chatId, $messageId, "DNS update command has been executed.");
+                if ($message['text'] === '\testDNS') {
+                    // Send initial message about processing start
+                    $initialReply = "Running the command.";
+                    $progressMessage = $this->sendReply($chatId, $messageId, $initialReply);
+                    dispatch(function () use ($progressMessage) {
+                        $dnsUpdateService = new DnsUpdateService(function ($message) {
+                            $this->info($message);
+                        });
+                        $dnsUpdateService->botDNSCheck($progressMessage);
+                    });
+                    $this->sendReply($chatId, $messageId, "DNS update command has been executed.");
                 } else {
                     $this->sendReply($chatId, $messageId, "No file received.");
                 }
@@ -91,10 +98,10 @@ class isegarobotController extends Controller
                 $this->sendReply($chatId, $messageId, "No text message received.");
             }
         }
-    
+
         return response()->json(['status' => 'ok']);
     }
-    
+
 
     private function sendReply($chatId, $messageId, $text)
     {
