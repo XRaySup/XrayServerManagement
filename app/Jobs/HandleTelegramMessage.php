ffe28f2ecda7336da6b8b22090736a1f3949d81a
XRaySup <xray.supp@gmail.com>
2024-12-22 02:50:23 +0000

    main

--- app/Jobs/HandleTelegramMessage.php
+++ app/Jobs/HandleTelegramMessage.php
@@ -9,6 +9,7 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Log;
 use App\Models\Server;
+use Telegram\Bot\Laravel\Facades\Telegram;
 
 class HandleTelegramMessage implements ShouldQueue
 {
@@ -16,6 +17,7 @@ class HandleTelegramMessage implements ShouldQueue
 
     protected $requestData;
     protected $botIdentifier;
+    private $telegram;
 
     /**
      * Create a new job instance.
@@ -26,6 +28,7 @@ class HandleTelegramMessage implements ShouldQueue
     {
         $this->requestData = $requestData;
         $this->botIdentifier = request()->query('bot', 'unknown_bot'); // Extract bot name from query parameter
+        
         Log::info('HandleTelegramMessage job created.');
     }
 
@@ -37,74 +40,110 @@ class HandleTelegramMessage implements ShouldQueue
     public function handle()
     {
         Log::info('HandleTelegramMessage job started.');
-        $message = $this->requestData['message'];
-        $botIdentifier = $this->botIdentifier; // Use the extracted bot name
-        $chatId = $message['chat']['id'];
-        $messageId = $message['message_id'];
-        $userName = $message['from']['username'] ?? 'Unknown';
-        $firstName = $message['from']['first_name'] ?? 'Unknown';
-        $lastName = $message['from']['last_name'] ?? 'Unknown';
-
-        // Log the received message for debugging
-        Log::info('Received message from bot ' . $botIdentifier . ': ', $this->requestData);
-
-        $adminIds = explode(',', env('TELEGRAM_XADMIN_IDS'));
-        if (!in_array((int) $chatId, $adminIds)) {
-            // Notify admins about the unauthorized attempt
-            foreach ($adminIds as $adminId) {
-                $this->sendReply(trim($adminId), null, "Non-admin user tried to interact: \nID: $chatId\nUsername: $userName\nName: $firstName $lastName");
-            }
-
-            // Send message to the non-admin user
-            $this->sendReply($chatId, $messageId, "You are not authorized to use this bot.");
-
-            // Return response after non-admin check
-            return;
+        switch ($this->botIdentifier) {
+            case 'Test':
+                $this->telegram = Telegram::bot($this->botIdentifier);
+                $this->handleFreeXrayBotMessage();
+            case 'free_xray_bot':
+                $this->telegram = Telegram::bot('FreeXrayBot');
+                $this->handleFreeXrayBotMessage();
+                break;
+            // case 'another_bot':
+            //     $this->telegram = Telegram::bot('AnotherBot');
+            //     $this->handleAnotherBotMessage();
+            //     break;
+            default:
+                Log::error('Unknown bot: ' . $this->botIdentifier);
         }
+        // $message = $this->requestData['message'];
+        // $botIdentifier = $this->botIdentifier; // Use the extracted bot name
+        // $chatId = $message['chat']['id'];
+        // $messageId = $message['message_id'];
+        // $userName = $message['from']['username'] ?? 'Unknown';
+        // $firstName = $message['from']['first_name'] ?? 'Unknown';
+        // $lastName = $message['from']['last_name'] ?? 'Unknown';
 
-        if (isset($message['text'])) {
-            if ($message['text'] === '/usage') {
-                $servers = Server::all();
-                // Prepare the table message in Markdown format
-                $reply = "*Remark* | *Usage (GB)*\n--- | ---\n";
-                $totalUsage = 0;
-                foreach ($servers as $server) {
-                    if ($server->status == "ONLINE") {
-                        $reply .= "{$server->remark} | *{$server->todayUsage}* \n";
-                        $totalUsage += $server->todayUsage;
-                    }
-                }
-                $reply .= "Total | *{$totalUsage}* \n";
-                try {
-                    $response = $this->telegram->sendMessage([
-                        'chat_id' => $chatId,
-                        'reply_to_message_id' => $messageId,
-                        'text' => $reply,
-                        'parse_mode' => 'Markdown'
-                    ]);
-                    return $response;
-                } catch (\Exception $e) {
-                    Log::error('Telegram API error: ' . $e->getMessage());
-                }
-            } else {
-                $this->sendReply($chatId, $messageId, "No file received.");
-            }
-        } else {
-            $this->sendReply($chatId, $messageId, "No text message received.");
-        }
+        // // Log the received message for debugging
+        // Log::info('Received message from bot ' . $botIdentifier . ': ', $this->requestData);
 
-        $text = $message['text'] ?? '';
+        // $adminIds = explode(',', env('TELEGRAM_XADMIN_IDS'));
+        // if (!in_array((int) $chatId, $adminIds)) {
+        //     // Notify admins about the unauthorized attempt
+        //     foreach ($adminIds as $adminId) {
+        //         $this->sendReply(trim($adminId), null, "Non-admin user tried to interact: \nID: $chatId\nUsername: $userName\nName: $firstName $lastName");
+        //     }
 
-        // Process the message
-        if ($text === '/start') {
-            $this->sendReply($chatId, $messageId, 'Welcome to the bot!');
-        } else {
-            $this->sendReply($chatId, $messageId, 'You said: ' . $text);
-        }
+        //     // Send message to the non-admin user
+        //     $this->sendReply($chatId, $messageId, "You are not authorized to use this bot.");
+
+        //     // Return response after non-admin check
+        //     return;
+        // }
+
+        // if (isset($message['text'])) {
+        //     if ($message['text'] === '/usage') {
+        //         $servers = Server::all();
+        //         // Prepare the table message in Markdown format
+        //         $reply = "*Remark* | *Usage (GB)*\n--- | ---\n";
+        //         $totalUsage = 0;
+        //         foreach ($servers as $server) {
+        //             if ($server->status == "ONLINE") {
+        //                 $reply .= "{$server->remark} | *{$server->todayUsage}* \n";
+        //                 $totalUsage += $server->todayUsage;
+        //             }
+        //         }
+        //         $reply .= "Total | *{$totalUsage}* \n";
+        //         try {
+        //             $response = $this->telegram->sendMessage([
+        //                 'chat_id' => $chatId,
+        //                 'reply_to_message_id' => $messageId,
+        //                 'text' => $reply,
+        //                 'parse_mode' => 'Markdown'
+        //             ]);
+        //             return $response;
+        //         } catch (\Exception $e) {
+        //             Log::error('Telegram API error: ' . $e->getMessage());
+        //         }
+        //     } else {
+        //         $this->sendReply($chatId, $messageId, "No file received.");
+        //     }
+        // } else {
+        //     $this->sendReply($chatId, $messageId, "No text message received.");
+        // }
+
+        // $text = $message['text'] ?? '';
+
+        // // Process the message
+        // if ($text === '/start') {
+        //     $this->sendReply($chatId, $messageId, 'Welcome to the bot!');
+        // } else {
+        //     $this->sendReply($chatId, $messageId, 'You said: ' . $text);
+        // }
     }
 
-    private function sendReply($chatId, $messageId, $text)
+    private function handleFreeXrayBotMessage()
     {
-        // Implement the sendReply method to send a message via the Telegram API
+        // Implement the handleFreeXrayBotMessage method
+        Log::info('Handling FreeXrayBot message.');
+        // Add your logic here
+        $this->sendReply('Hello from FreeXrayBot!');
+    }
+
+    private function sendReply($text)
+    {
+        $message = $this->requestData['message'];
+        $chatId = $message['chat']['id'];
+        $messageId = $message['message_id'];
+
+        try {
+            $response = $this->telegram->sendMessage([
+                'chat_id' => $chatId,
+                'reply_to_message_id' => $messageId,
+                'text' => $text,
+            ]);
+            return $response;
+        } catch (\Exception $e) {
+            Log::error('Telegram API error: ' . $e->getMessage());
+        }
     }
 }
