94658195380810e4727ae3d8a6fd94fbef21bc44
XRaySup <xray.supp@gmail.com>
2025-06-28 18:38:32 +0100

    skip ips

--- app/Services/DnsUpdateService.php
+++ app/Services/DnsUpdateService.php
@@ -353,7 +353,7 @@ class DnsUpdateService
 
                     $skipIps = array_map('trim', explode(',', config('app.skip_ips', '')));
                     if (in_array($ip, $skipIps)) {
-                        $reply .= "IP $ip is in the skip list. No action taken.\n";
+                        $reply .= "IP $ip is in the single ip skip list. No action taken.\n";
                         $this->logAndInfo("IP $ip is in the single ip skip list. No action taken.");
                         return $reply;
                     }
